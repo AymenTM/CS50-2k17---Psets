06a7c27d5d5d7cf0d300c41d1df50eab79a9b515
nothingToSeeHere <36056254+notAymen@users.noreply.github.com>
2018-07-16 19:59:10 +0300

    Update selectionSort.c

--- Problem Set 3/Sorting Algorithms/selectionSort.c
+++ Problem Set 3/Sorting Algorithms/selectionSort.c
@@ -1,13 +1,12 @@
 
-// PROTOTYPES
+// PROTOTYPE
 void selectionSort(int *array, int sizeOfArray);
-void swap(int *var1, int *var2);
 
 
 // Selection Sort
 void selectionSort(int *array, int sizeOfArray) {
 
-    int i, startIndex = 0, currentSmallest;
+    int i, startIndex = 0, currentSmallest, temporary;
 
         // Increment the index at which we start by 1 After Each Sort
         for (startIndex = 0; startIndex < sizeOfArray - 1; startIndex++) {
@@ -20,15 +19,13 @@ void selectionSort(int *array, int sizeOfArray) {
                 // If a smaller Int is Found
                 if (array[i] < array[currentSmallest])
 
-                    // Keep track of where that smallest int was found
+                    // Keep track of where the smallest int was found
                     currentSmallest = i;
             }
 
-            // Put the smallest int found at the beginning of the sorted list
-            swap(&array[startIndex], &array[currentSmallest]);
+            // Put the smallest int found at the beginning
+            temporary = array[startIndex]; 
+            array[startIndex] = array[currentSmallest];
+            array[currentSmallest] = temporary;
         }
 }
-
-
-// Swaps the Contents of two Variables
-void swap(int *var1, int *var2) { int tempory = *var1; *var1 = *var2; *var2 = tempory; }
