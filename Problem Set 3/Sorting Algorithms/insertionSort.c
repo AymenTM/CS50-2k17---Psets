0446267f2f2652b1097fe520bdd2f6e368a35b14
nothingToSeeHere <36056254+notAymen@users.noreply.github.com>
2018-07-16 19:49:26 +0300

    Create insertionSort.c

---
+++ Problem Set 3/Sorting Algorithms/insertionSort.c
@@ -0,0 +1,25 @@
+
+// PROTOTYPES
+void selectionSort(int *array, int sizeOfArray);
+
+
+// Insertion Sort
+void insertionSort(int *array, int sizeOfArray) {
+
+    int i, grab, sortd;
+
+        // Scan Through Unsorted Array
+        for (i = 1; i < sizeOfArray; i++) {
+
+            // Grab value at index 'i'
+            grab = array[i];
+
+            // Make place for value at index 'i' in sorted list
+            for (sortd = i - 1; sortd >= 0 && array[sortd] > grab; sortd--)
+
+                array[sortd+1] = array[sortd];
+
+            // Insert value at index 'i'
+            array[sortd+1] = grab;
+        }
+}
