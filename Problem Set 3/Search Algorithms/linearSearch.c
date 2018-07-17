7fd6e065c4125fefb6c92e575c055a683e8e72f3
nothingToSeeHere <36056254+notAymen@users.noreply.github.com>
2018-07-17 20:47:09 +0300

    Update linearSearch.c

--- Problem Set 3/Search Algorithms/linearSearch.c
+++ Problem Set 3/Search Algorithms/linearSearch.c
@@ -3,18 +3,18 @@
 int linearSearch(int key, int *array, int sizeOfArray);
 
 
-// LINEAR SEARCH - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-
+// Linear Search
 int linearSearch(int key, int *array, int sizeOfArray) {
-
-    int i;
-
-        for (i = 0; i < sizeOfArray; i++)
-
+        
+        // Go through each element of the array
+        for (int i = 0; i < sizeOfArray; i++) 
+            
+            // If key is found
             if (array[i] == key)
-
+            
+                // Return its index
                 return i;
-
+        
+        // Else, signal that key was not found
         return -1;
 }
