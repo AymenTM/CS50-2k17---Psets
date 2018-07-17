82fbe84abadc5c9eae66f6eea508ba8cedde36fd
nothingToSeeHere <36056254+notAymen@users.noreply.github.com>
2018-07-17 20:53:35 +0300

    Update and rename binarySearch to binarySearch.c

---
+++ Problem Set 3/Search Algorithms/binarySearch.c
@@ -0,0 +1,35 @@
+
+// PROTOTYPE
+int binarySearch(int key, int *array, int sizeOfArray);
+
+
+// Binary Search
+int binarySearch(int key, int *array, int sizeOfArray) {
+
+    // Define starting index (mid array) & Current Number of Elements
+    int index = ceil(sizeOfArray * 0.5) - 1, elements = sizeOfArray;
+
+        // While there still are elements to be investigated
+        while (elements) {
+
+            // If element matches key --> Return its index
+            if (array[index] == key) return index;
+
+            // (Element Bigger than Key); Divide accordingly
+            else if (array[index] > key) {
+
+                elements = floor(elements * 0.5) + (elements%2 - 1);
+                index = index+1 - ceil(elements/2) + ((elements/2%2) - 1) - 1;
+            }
+
+            // (Element Smaller than Key); Divide accordingly
+            else (array[index] < key) {
+
+                elements = ceil(elements * 0.5) - (elements%2);
+                index = index+1 + ceil(elements/2) - (elements/2%2);
+            }
+        }
+        
+        // Else, signal that key was not found        
+        return -1;
+}
