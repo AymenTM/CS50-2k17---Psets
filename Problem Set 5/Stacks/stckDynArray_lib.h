1f8847f8e17d4426153e56fe93dbb2a2155b84ea
nothingToSeeHere <36056254+notAymen@users.noreply.github.com>
2018-07-24 22:49:53 +0300

    Add files via upload

---
+++ Problem Set 5/Stacks/stckDynArray_lib.h
@@ -0,0 +1,72 @@
+//
+// Dynamic Stack Arrays Library/ToolKit (Header)
+// - - - - - - - - - - - - - - - -
+
+
+// Datatype carried by array (int)
+#define VALUE int
+
+
+// Stack Datatype Structure
+typedef struct {
+    VALUE *array;
+    int top;
+} stack;
+
+
+// Prototypes
+// int push(VALUE val, stack *ptr);             //  <--- Push (i.e add a value to the stack).
+// VALUE pop(stack *ptr);                       //  <--- Pop (i.e grab the most recent value added to the stack).
+// void printStack(stack *stack)                //  <--- Prints stack that is currently in use.
+
+
+/*Define CAPACITY (i.e the amount of bytes you allocate to the array, and declare stack(s) in main) */
+
+
+// Push (i.e add a value to the stack) - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+int push(VALUE item, stack *stack) {
+
+    if (!stack) {
+
+        fprintf(stderr, "\nPush failed. --> Pointer to stack points to NULL.\n");
+        return 1;
+    }
+
+    stack->array[stack->top] = item;
+    stack->top++;
+
+    // Successful Push
+    return 0;
+}
+
+
+
+// Pop (i.e remove/grab the most recent element from the top of the stack) - - - - - - - - - -
+
+VALUE pop(stack *stack) {
+
+    stack->top--;
+    return stack->array[stack->top];
+}
+
+
+
+// Prints Stack - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+void printStack(stack *stack) {
+
+    if (!stack) {
+
+        fprintf(stderr, "\nPrinting failed. --> Pointer to stack points to NULL.\n");
+        return;
+    }
+
+    printf("\n\nStack: ");
+    for (size_t i = 0; i < stack->top; i++) {
+
+        printf("%i, ", stack->array[i]);
+    } printf("\n\n");
+
+    return;
+}
