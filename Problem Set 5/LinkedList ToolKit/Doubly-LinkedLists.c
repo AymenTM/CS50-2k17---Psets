893c99d47d6fc9002a98ddc27b3f2759dfd3fbeb
nothingToSeeHere <36056254+notAymen@users.noreply.github.com>
2018-07-24 11:47:59 +0300

    Update Doubly-LinkedLists.c

--- Problem Set 5/LinkedList ToolKit/Doubly-LinkedLists.c
+++ Problem Set 5/LinkedList ToolKit/Doubly-LinkedLists.c
@@ -1,6 +1,6 @@
 
-// Doubly-Linked List ToolKit (Library)
-- - - - - - - - - - - - - - - - - - - - - 
+//   Doubly-Linked List ToolKit (Header)
+// - - - - - - - - - - - - - - - - - - - - -
 
 
 // Datatype carried by Node(s)
@@ -16,17 +16,21 @@ typedef struct preNode {
 
 
 // Global Variable(s)
-node *listN = malloc(sizeof(node));
-node *probe = malloc(sizeof(node));
+node *probe;
+
+// node *listN;
+// .
+// .
+// .
 
 
 // // Prototypes
 // node *create(VALUE item);
-// void insert(VALUE item, node *list);
-// void insertOnce(VALUE item, node *list);
-// bool find(VALUE item, node *list);
-// void delete(VALUE item, node *list);
-// void destroy(node *list);
+// int insert(VALUE item, node *list);
+// int insertOnce(VALUE item, node *list);
+// int find(VALUE item, node *list);
+// int delete(VALUE item, node *list);
+// int destroy(node *list);
 // void printList(node *list);
 
 
@@ -39,13 +43,14 @@ node *create(VALUE item) {
     node *genesis_Node = malloc(sizeof(node));
     if (!genesis_Node) {
 
-        fprintf(stderr, "\nCould not create genesis node. Out of Memory.\nCreation of Singly-Linked List Failed.\n");
+        fprintf(stderr, "\nCreation of Singly-Linked List Failed. --> Could not create genesis node. Out of Memory.\n");
         return NULL;
     }
 
     // Initialize the Node
     genesis_Node->item = item;
     genesis_Node->next = NULL;
+    genesis_Node->prev = NULL;
 
     // Return a Pointer to the Node/List
     return genesis_Node;
@@ -53,23 +58,64 @@ node *create(VALUE item) {
 
 
 
-// Inserts a Item in a Doubly-Linked List ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈
+// Linear Search (Sgly/Dbly) Linked List for an Item; Boolean Return & Probe Points to Item - - -
+
+node *find(VALUE item, node *list) {
+
+    if (!list) {
+
+        fprintf(stderr, "\nSearch Failed. --> Head node points to NULL.\n");
+        return NULL;
+    }
+
+    // Place Probe at the Beginning of the List
+    probe = list;
+
+    // Till the Probe Reaches the end of the Linked List
+    while (probe != NULL) {
+
+        // Check if the Element Matches Key (return true if it does)
+        if (probe->item == item) return probe;
+
+        // Else Check next Item in the List
+        else probe = probe->next;
+    }
+
+    // If Element Does Not Exist
+    fprintf(stderr, "\nItem Not Found.\n");
+    return NULL;
+}
+
+
+// Used in del(), same as find() except doesn't print to stderr that item wasnt found, cuz del() prints it instead, otherwise it would print the msg twice
+node *dfind(VALUE item, node *list) {
+    if (!list) { fprintf(stderr, "\nSearch Failed. --> Head node points to NULL.\n"); return NULL; }
+    probe = list;
+    while (probe != NULL) {
+        if (probe->item == item) return probe;
+        else probe = probe->next;
+    } return NULL;
+}
+
+
+
+// Inserts a Item in a Doubly-Linked List - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 // Inserts whether item duplicate(s) exist(s) or not
-void insert(VALUE item, node *list) {
+node *insert(VALUE item, node *list) {
 
     // Allocate Space for Node
     node *new_Node = malloc(sizeof(node));
     if (!new_Node) {
 
-        fprintf(stderr, "\nCould not create node. Out of Memory.\nInsertion Failed.\n");
-        return NULL;
+        fprintf(stderr, "\nInsertion Failed. --> Could not create node. Out of Memory.\n");
+        return list;
     }
 
     if (!list) {
 
-        fprintf(stderr, "\nHead node points to NULL.\nInsertion Failed.\n");
-        return;
+        fprintf(stderr, "\nInsertion Failed. --> Head node points to NULL.\n");
+        return list;
     }
 
     // Initialize Node Accordingly & Update the Former Head Node
@@ -80,32 +126,30 @@ void insert(VALUE item, node *list) {
     // Updating Former Head Node
     list->prev = new_Node;
 
-    // Updating Head Pointer
-    list = new_Node;
-
     // Insertion Successful
-    return;
+    return new_Node;
 }
 
 
-// Inserts ONLY if No Item duplicate(s) exist(s)
-void insertOnce(VALUE item, node *list) {
+// Inserts ONLY if No Item duplicate(s) exist(s) - - - - - - - - - - - - - - - - - - - - - - - - -
+node *insertOnce(VALUE item, node *list) {
 
-    if (find(item, list) == true) {
+    if (find(item, list) == 0) {
 
         // Allocate Space for New Node
         node *new_Node = malloc(sizeof(node));
         if (!new_Node) {
 
-            fprintf(stderr, "\nCould not create node. Out of Memory.\nInsertion Failed.\n");
-            return NULL;
+            fprintf(stderr, "\nInsertion Failed. --> Could not create node. Out of Memory.\n");
+            return list;
         }
 
         if (!list) {
 
-            fprintf(stderr, "\nHead node points to NULL.\nInsertion Failed.\n");
-            return;
+            fprintf(stderr, "\nInsertion Failed. --> Head node points to NULL.\n");
+            return list;
         }
+
 
         // Initialize Node Accordingly & Update the Former Head Node
         new_Node->prev = list->prev;
@@ -115,146 +159,118 @@ void insertOnce(VALUE item, node *list) {
         // Updating Former Head Node
         list->prev = new_Node;
 
-        // Updating Head Pointer
-        list = new_Node;
-
         // Insertion Successful
-        return;
+        return new_Node;
     }
 
     // Insertion unSuccessful
-    fprintf(stderr, "\nItem duplicate(s) exists.\nInsertion Failed.\n");
-    return;
+    fprintf(stderr, "\nInsertion Failed. --> Item duplicate(s) exists.\n");
+    return list;
 }
 
 
 
-// Linear Search (Sgly/Dbly) Linked List for an Item; Boolean Return & Probe Points to Item ≈ ≈
+// Deletes a Specified Item from a Doubly-Linked List - - - - - - - - - - - - - - - - - - - - - -
 
-bool find(VALUE item, node *list) {
+int del(VALUE item, node *list) {
 
-    if (!list) {
+    if (list == NULL) {
 
-        fprintf(stderr, "\nHead node points to NULL.\nSearch Failed.\n");
-        return;
+        fprintf(stderr, "\nDeletion Failed. --> Head node points to NULL.\n");
+        return 1;
     }
 
-    // Place Probe at the Beginning of the List
-    probe = list;
+    if (dfind(item, list) == NULL) {
 
-    // Till the Probe Reaches the end of the Linked List
-    while (probe) {
-
-        // Check if the Element Matches Key (return true if it does)
-        if (probe->item == item) return true;
-
-        // Else Check next Item in the List
-        else probe = probe->next;
+        fprintf(stderr, "\nDeletion Failed. --> Item Not Found.\n");
+        return 2;
     }
 
-    // If Element Does Not Exist
-    fprintf(stderr, "\nItem Not Found.\n");
-    return false;
+    node *deleteNode = probe;
+
+    if (deleteNode == NULL) {
+
+        fprintf(stderr, "\nDeletion Failed. --> Could not create deleteNode. Out of Memory.\n");
+        return 3;
+    }
+
+
+    // Only Nodes that don't have a next & a prev pointer pointing to NULL
+    if (deleteNode->next != NULL && deleteNode->prev != NULL) {
+
+        deleteNode->next->prev = deleteNode->prev;
+        deleteNode->prev->next = deleteNode->next;
+    }
+
+    // Only if DeleteNode is the Head node of the list (makes the previous pointer of the node after the head node point to NULL, which after deletion will become the head node)
+    if (deleteNode == list) {
+        list = deleteNode->next; deleteNode->next->prev = deleteNode->prev;
+    }
+
+    // Only last Node of the list (makes the before last node next pointer point to NULL, which after deletion will become the last node of the list)
+    if (deleteNode->next == NULL) {
+        deleteNode->prev->next = deleteNode->next;
+    }
+
+
+    // Delete Node & Return
+    free(deleteNode);
+    return 0;
 }
 
 
 
-// Deletes a Specified Item from a Doubly-Linked List ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈
-
-void delete(VALUE item, node *list) {
-
-    if (find(item, list) == true) {
-
-        // Declare DeleteNode that will contain a pointer the node we want to delete
-        node *deleteNode = malloc(sizeof(node));
-        if (!deleteNode) {
-
-            fprintf(stderr, "\nCould not create node. Out of Memory.\nDeletion Failed.\n");
-            return;
-        }
-
-        if (!list) {
-
-            fprintf(stderr, "\nHead node points to NULL.\nDeletion Failed.\n");
-            return;
-        }
-
-        // At this point, probe will point to the node we want to delete, so...
-        deleteNode = probe;
-
-
-        // Only if DeleteNode is the Head node of the list (makes the previous pointer of the node after the head node point to NULL, which after deletion will become the head node)
-        if (deleteNode == list) { list = deleteNode->next; }
-
-        // Only Nodes that don't have a next pointer pointing to NULL
-        if (deleteNode->next != NULL) { deleteNode->next->prev = deleteNode->prev; }
-
-        // Only last Node of the list (makes the before last node next pointer point to NULL, which after deletion will become the last node of the list)
-        if (deleteNode->next == NULL) { deleteNode->prev->next = deleteNode->next; }
-
-
-        // Delete Node & Return
-        free(deleteNode);
-        return;
-    }
-
-    // If Item isn't found
-    fprintf(stderr, "\nItem Not Found.\nDeletion Failed.\n");
-    return;
-}
-
-
-
-// Destroys an entire (Sgly/Dbly) Linked List ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈
+// Destroys an entire (Sgly/Dbly) Linked List - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 // Recursive Implementation
-void destroy(node *list) {
+int destroy(node *list) {
 
-    if (!(list->next))
-        free(list);
+    if (list->next == NULL) free(list);
 
-    else
-        destroy(list->next);
+    else { destroy(list->next); free(list); }
 
-    return;
+    // Deletion Successful
+    return 0;
 }
 
 
-// Iterative Implementation
-void destroy(node *list) {
+// // Iterative Implementation
+// int destroy(node *list) {
 
-    if (!list) {
+//     if (!list) {
 
-        fprintf(stderr, "\nHead node points to NULL.\nDestruction Failed.\n");
-        return;
-    }
+//         fprintf(stderr, "\nDestruction Failed. --> Head node points to NULL.\n");
+//         return 1;
+//     }
 
-    node *next = malloc(sizeof(node));
-    probe = list;
-    next = probe;
+//     probe = list;
 
-    if (!next) {
+//     while (list != NULL) {
 
-        fprintf(stderr, "\nCould not create node.\nDestruction Failed.\n");
-        return;
-    }
+//         probe = list->next;
+//         free(list);
+//         list = probe;
 
-    while (probe) {
+//     }
 
-        next = probe->next;
-        free(probe);
-        probe = next;
-    }
-
-    return;
-}
+// // Deletion Successful
+//     return 0;
+// }
 
 
 
-// Prints a given (Sgly/Dbly) Linked List ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈
+// Prints a given (Sgly/Dbly) Linked List  - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 void printList(node *list) {
-    for (probe = list; probe; probe = probe->next) { printf("%.0f, ", probe->item); } return;
+
+    printf("\n\n---> ");
+    for (probe = list; probe != NULL; probe = probe->next) {
+
+        printf("%d, ", probe->item);
+    }
+    printf("\n\n");
+
+    return;
 }
 
-// ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈ ≈
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
