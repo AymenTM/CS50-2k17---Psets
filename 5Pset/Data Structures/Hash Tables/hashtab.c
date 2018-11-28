3c4730abf03562094b4493c83a8bbb0c02c90c6a
AymenTM <idev.aymen@gmail.com>
2018-11-28 14:15:59 -0800

    Unimportant Change or Tiny Tweak

--- 5Pset/Data Structures/Hash Tables/hashtab.c
+++ 5Pset/Data Structures/Hash Tables/hashtab.c
@@ -18,15 +18,10 @@
 #endif
 
 
-
-
 /**
 	DEPENDENCIES:	none.
-
 	DESCRIPTION:	.
-
 	RETURN VALUES:	Returns 0 upon success; otherwise -1.
-
 	SEARCH TAGS:	ft hashtable_insert_data
 */
 hashtable_insert_data()
@@ -34,15 +29,10 @@ hashtable_insert_data()
 
 }
 
-
-
 /**
 	DEPENDENCIES:	none.
-
 	DESCRIPTION:	.
-
 	RETURN VALUES:	Returns 0 upon success; otherwise -1.
-
 	SEARCH TAGS:	ft hashtable_delete_data
 */
 hashtable_delete_data()
@@ -50,15 +40,10 @@ hashtable_delete_data()
 
 }
 
-
-
 /**
 	DEPENDENCIES:	none.
-
 	DESCRIPTION:	.
-
 	RETURN VALUES:	Returns 0 upon success; otherwise -1.
-
 	SEARCH TAGS:	ft hashtable_destroy
 */
 hashtable_destroy()
@@ -66,15 +51,10 @@ hashtable_destroy()
 
 }
 
-
-
 /**
 	DEPENDENCIES:	none.
-
 	DESCRIPTION:	.
-
 	RETURN VALUES:	Returns 0 upon success; otherwise -1.
-
 	SEARCH TAGS:	ft hashtable_fetch_data
 */
 hashtable_fetch_data()
@@ -82,15 +62,10 @@ hashtable_fetch_data()
 
 }
 
-
-
 /**
 	DEPENDENCIES:	none.
-
 	DESCRIPTION:	clears node value if it exists.
-
 	RETURN VALUES:	Returns 0 upon success; otherwise -1.
-
 	SEARCH TAGS:	ft hashtable_clear_val
 */
 hashtable_clear_val()
@@ -98,15 +73,10 @@ hashtable_clear_val()
 
 }
 
-
-
 /**
 	DEPENDENCIES:	none.
-
 	DESCRIPTION:	.
-
 	RETURN VALUES:	Returns 0 upon success; otherwise -1.
-
 	SEARCH TAGS:	ft hashtable_realloc
 */
 t_hashtable		*hashtable_realloc()
@@ -114,15 +84,10 @@ t_hashtable		*hashtable_realloc()
 
 }
 
-
-
 /**
 	DEPENDENCIES:	none.
-
 	DESCRIPTION:	.
-
 	RETURN VALUES:	Returns 0 upon success; otherwise -1.
-
 	SEARCH TAGS:	ft hashtable_dealloc
 */
 t_hashtable		*hashtable_dealloc()
@@ -130,15 +95,10 @@ t_hashtable		*hashtable_dealloc()
 
 }
 
-
-
 /**
 	DEPENDENCIES:	none.
-
 	DESCRIPTION:	.
-
 	RETURN VALUES:	Returns 0 upon success; otherwise -1.
-
 	SEARCH TAGS:	ft hashtable_size
 */
 unsigned int	hashtable_size(const t_hashtable *table)
@@ -146,15 +106,10 @@ unsigned int	hashtable_size(const t_hashtable *table)
 
 }
 
-
-
 /**
 	DEPENDENCIES:	none.
-
 	DESCRIPTION:	.
-
 	RETURN VALUES:	Returns 0 upon success; otherwise -1.
-
 	SEARCH TAGS:	ft hashtable_sizeof
 */
 size_t			hashtable_sizeof(const t_hashtable *table)
