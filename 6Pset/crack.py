d26533088c468fd0a346a06a3e98247e2a7e3435
AymenTM <idev.aymen@gmail.com>
2018-12-06 10:36:58 -0800

    Unimportant Change or Tiny Tweak

--- 6Pset/crack.py
+++ 6Pset/crack.py
@@ -10,50 +10,54 @@ from sys import argv
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 total_chars = 0
 
+
 def main():
-	if len(argv) is 2:
-		if crack(argv[1]):
-			return (0)
-		else:
-			print('Uncrackable!')
-			return (1)
-	else:
-		print('Usage: python crack.py hash')
-		return (1)
+    if len(argv) is 2:
+        if crack(argv[1]):
+            return (0)
+        else:
+            print('Uncrackable!')
+            return (1)
+    else:
+        print('Usage: python crack.py hash')
+        return (1)
+
 
 # Tries every possible permutation for the current index.
 def crack_furthur(hsh, passwd, salt, index):
-	found = 0
-	for c in alphabet:
-		passwd[index] = c
-		if total_chars > index:
-			found = crack_furthur(hsh, passwd, salt, index + 1)
-			if found:
-				return (1)
-		elif crypt(''.join(passwd), salt) == hsh:
-			print(''.join(passwd))
-			return (1)
-	return (0)
+    found = 0
+    for c in alphabet:
+        passwd[index] = c
+        if total_chars > index:
+            found = crack_furthur(hsh, passwd, salt, index + 1)
+            if found:
+                return (1)
+        elif crypt(''.join(passwd), salt) == hsh:
+            print(''.join(passwd))
+            return (1)
+    return (0)
+
 
 # Tries every possible permutation for the index 0 when
 # the password is at a length of 1, 2, 3, 4 & 5.
 def crack(hsh):
-	global total_chars
-	salt = hsh[:2]
-	passwd = ['', '', '', '', '']
-	found = 0
-	while total_chars < 5:
-		for c in alphabet:
-			passwd[0] = c
-			if total_chars > 0:
-				found = crack_furthur(hsh, passwd, salt, 1)
-				if found:
-					return (1)
-			elif crypt(''.join(passwd), salt) == hsh:
-				print(''.join(passwd))
-				return (1)
-		total_chars += 1
-	return (0)
+    global total_chars
+    salt = hsh[:2]
+    passwd = ['', '', '', '', '']
+    found = 0
+    while total_chars < 5:
+        for c in alphabet:
+            passwd[0] = c
+            if total_chars > 0:
+                found = crack_furthur(hsh, passwd, salt, 1)
+                if found:
+                    return (1)
+            elif crypt(''.join(passwd), salt) == hsh:
+                print(''.join(passwd))
+                return (1)
+        total_chars += 1
+    return (0)
+
 
 if __name__ == '__main__':
-	main()
+    main()
--- 6Pset/mario.py
+++ 6Pset/mario.py
@@ -4,26 +4,30 @@
 # ======================================= #
 # * * * * * * * * * * * * * * * * * * * * #
 
+
 def main():
-	height = int(input("Height: "))
-	while height < 1 or height > 100:
-		height = int(input("Height: "))
-	put_pyramid(height)
+    height = int(input("Height: "))
+    while height < 1 or height > 100:
+        height = int(input("Height: "))
+    put_pyramid(height)
+
 
 def put_pyramid(height):
-	i = 0
-	for y in range(height):
-		i += 1
-		hashes = height - (height - i)
-		spaces = height - hashes
-		for m in range(spaces):
-			print(" ", end="")
-		for n in range(hashes):
-			print("#", end="")
-		print("  ", end="")
-		for n in range(hashes):
-			print("#", end="")
-		print()
+    i = 0
+    for y in range(height):
+        i += 1
+        hashes = height - (height - i)
+        spaces = height - hashes
+        for m in range(spaces):
+            print(" ", end="")
+        for n in range(hashes):
+            print("#", end="")
+        print("  ", end="")
+        for n in range(hashes):
+            print("#", end="")
+        print()
+
 
 if __name__ == '__main__':
-	main()
+    main()
+
