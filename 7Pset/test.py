564ba61fe48e3db552ee68a6745bccd1d963efde
AymenTM <idev.aymen@gmail.com>
2018-12-07 23:47:02 -0800

    Unimportant Change or Tiny Tweak

--- 7Pset/test.py
+++ 7Pset/test.py
@@ -1,23 +1,24 @@
 # Testing stuff
 
-
 def main():
 
 	students_list = []
 
 	# Get Info of 3 students
 	for i in range(3):
+		print()
 
 		# Get Student Info
 		name = str(input('Name: '))
 		age = str(input('Age: '))
 		nationality = str(input('Nationality: '))
 
-		# Append Student Object to List
+		# Append Student Object to List (this is a dict variable named 'student')
 		student = {'Name': name, 'Age': age, 'Nationality': nationality}
 		students_list.append(student)
 
 	# Print Info for every Student in the List
+	print()
 	print_list_of_dicts(students_list)
 
 
