fb76dd88bfa2181f7bf9fc28300a09217b9ae634
AymenTM <idev.aymen@gmail.com>
2018-12-18 11:52:58 -0800

    Unimportant Change or Tiny Tweak

--- 7Pset/playground.py
+++ 7Pset/playground.py
@@ -1,10 +1,9 @@
 
-import datetime
-import pytz
+		with open('file.txt', 'r') as fd:
 
-dt = datetime.datetime.strptime(date_string, format)
+		    for line in fd:
+		        print(line, end='')
 
-dt_str = 'July 26, 2016'
-dt = datetime.datetime.strptime(dt_str, '%B %d, %Y')
-
-print(dt)
+	 	>> > 'this is Line #1'
+	    >>> 'this is Line #2'
+	    >>> 'this is Line #3'
