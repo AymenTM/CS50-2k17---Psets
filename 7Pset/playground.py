d3f0ae82bc49cf456ad28f9199bdb24808045dfd
AymenTM <idev.aymen@gmail.com>
2018-12-17 21:23:06 -0800

    Unimportant Change or Tiny Tweak

--- 7Pset/playground.py
+++ 7Pset/playground.py
@@ -7,4 +7,4 @@ dt = datetime.datetime.strptime(date_string, format)
 dt_str = 'July 26, 2016'
 dt = datetime.datetime.strptime(dt_str, '%B %d, %Y')
 
-print(dt) >> > '2016-07-26 00:00:00'
+print(dt)
