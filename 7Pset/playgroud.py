cf8b1582adf18d9e0d2450586f5d1e4b7cd1686f
AymenTM <idev.aymen@gmail.com>
2018-12-24 12:05:42 -0800

    Unimportant Change or Tiny Tweak

--- 7Pset/playgroud.py
+++ 7Pset/playgroud.py
@@ -1,16 +1,63 @@
 
+import memory_profiler
+import random
+import time
 
-person = {'name': 'Jess', 'age': 23, 'job': 'Programmer'}
-person = {'name': 'Jess', 'age': 23}
+names = ['John', 'Louis', 'Corey', 'John', 'Adam', 'Steve', 'Rick', 'Thomas', ]
+majors = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
 
-# LBYL (Non-Pythonic)
-if 'name' in person and 'age' in person and 'job' in person:
-    print("I'm {name}. I'm {age} years old and I am a {job}".format(**person))
-else:
-    print('Missing some keys')
+print(f'Memory (Before): {memory_profiler.memory_usage()}Mb')
 
-# EAFP (Pythonic)
-try:
-    print("I'm {name}. I'm {age} years old and I am a {job}".format(**person))
-except KeyError as e:
-    print("Missing {} key".format(e))
+
+def people_list(num_people):
+    result = []
+    for i in range(num_people):
+        person = {
+            'id': i,
+            'name': random.choice(names),
+            'major': random.choice(majors)
+        }
+        result.append(person)
+    return (result)
+
+
+def people_generator(num_people):
+    for i in range(num_people):
+        person = {
+            'id': i,
+            'name': random.choice(names),
+            'major': random.choice(majors)
+        }
+    yield person
+
+# Testing Lists
+# t1 = time.perf_counter()
+# people = people_list(1000000)
+# t2 = time.perf_counter()
+
+
+# Testing Generators
+t1 = time.perf_counter()
+people = people_generator(1000000)
+t2 = time.perf_counter()
+
+print(f'Memory (Before): {memory_profiler.memory_usage()}Mb')
+print(f'Took {t2-t1} Seconds')
+
+
+# # 			Credits: Corey Schafer (Youtube)
+
+
+# 			# Lists
+# 			'Memory (Before): [12.21484375]Mb'
+# 			'Memory (Before): [276.66796875]Mb'
+# 			'Took 5.125476114 Seconds'
+
+
+# #						        V.S
+
+
+# 			# Generators
+# 			'Memory (Before): [12.1875]Mb'
+# 			'Memory (Before): [12.2109375]Mb'
+# 			'Took 3.597999999993551e-06 Seconds'
