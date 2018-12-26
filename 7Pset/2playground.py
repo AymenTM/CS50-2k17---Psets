245e1f405e204c3cc9e8cefa4a0c3e68a0a78117
AymenTM <idev.aymen@gmail.com>
2018-12-26 14:12:44 -0800

    Unimportant Change or Tiny Tweak

--- 7Pset/2playground.py
+++ 7Pset/2playground.py
@@ -1,22 +1,24 @@
 
-from functools import wraps
+    from functools import wraps
+
+    def logger(func):
+
+        import logging
+        logging.basicConfig(filename=f'{func.__name__}.log', level=logging.INFO)
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+
+            logging.info(f'Ran with args: {args}, and kwargs: {kwargs}')
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    @logger
+    def greet_user(greeting, username):
+        print(f'{greeting}, {username}')
 
 
-def do_twice(func):
-
-    @wraps(func)
-    def wrapper_do_twice(*args, **kwargs):
-        func(*args, **kwargs)
-        func(*args, **kwargs)
-
-    return wrapper_do_twice
-
-
-@do_twice
-def say_msg(msg):
-    print(msg)
-
-
->>> say_msg('Decorators are cool!')
-'Decorators are cool!'
-'Decorators are cool!'
+greet_user('Hello', 'Tim')
+greet_user('Hi', 'John')
+greet_user('Welcome back', 'Louis')
