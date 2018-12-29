48be970763011894452bedbeb8d95fbd95479b98
AymenTM <idev.aymen@gmail.com>
2018-12-28 21:16:12 -0800

    Unimportant Change or Tiny Tweak

--- 7Pset/playground.py
+++ 7Pset/playground.py
@@ -1,16 +1,20 @@
 
 class Employee:
 
+    num_employees = 0
+    raise_amount = 1.04
+
     def __init__(self, first, last, pay):
 
         self.first = first
         self.last = last
         self.pay = pay
-        self.email = f'{first}.{last}@company.com'
+        self.email = f'{first.lower()}.{last.lower()}@company.com'
+
+        Employee.num_employees += 1
 
     def fullname(self):
-
         return f'{self.first} {self.last}'
 
-
-emp_1 = Employee('James', 'Lorry', 60000)
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
