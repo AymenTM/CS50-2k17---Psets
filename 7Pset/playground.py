c5b6d8f62862a741408ee3a50551bda700eb9bd4
AymenTM <idev.aymen@gmail.com>
2019-01-05 21:08:16 -0800

    Learning New Stuff

--- 7Pset/playground.py
+++ 7Pset/playground.py
@@ -1,18 +1,86 @@
 
-# Image Manipulation using Pillow's Image Module
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
 
-from PIL import Image
-import os
 
-os.mkdir('./1000/')
-new_size = (1000, 1000)
+# s1 = {1, 2, 3, 4, 5}
+# s2 = {11, 12, 13, 14, 15}
 
-for file in os.listdir(path='.'):
+# s1.add(6)
+# s1.update([7, 8, 9, 10], s2)
 
-    if file.endswith('.jpeg'):
+# print(s1)
 
-        fn, fx = os.path.splitext(file)
 
-        img = Image.open(file)
-        img.thumbnail(new_size)
-        img.save(f'1000/{fn}_1000.png')
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# s1 = {1, 2, 3, 4, 5}
+
+# s1.remove(4)
+# s1.discard(5)
+
+# print(s1)
+
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# s1 = {1, 2, 3}
+# s2 = {2, 3, 4}
+# s3 = {3, 4, 5}
+
+# s4 = s1.intersection(s2)
+# s5 = s1.intersection(s2, s3)
+
+# print(s4)
+# print(s5)
+
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+
+# s1 = {1, 2, 3}
+# s2 = {2, 3, 4}
+# s3 = {3, 4, 5}
+
+# s4 = s1.difference(s2)
+
+
+# print(s4)
+
+# {1}
+
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# s1 = {1, 2, 3}
+# s2 = {2, 3, 4}
+
+# s3 = s1.symmetric_difference(s2)
+
+# print(s3)
+
+# {1, 4}
+
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+employees = ['Corey', 'Jim', 'Steven', 'April', 'Judy', 'Jenn', 'John', 'Jane']
+
+gym_members = ['Corey', 'April', 'John']
+
+developpers = ['Corey', 'Steven', 'April', 'Judy', 'Jane']
+
+
+# gym member and developper
+result = set(gym_members).intersection(developpers)
+print(result)
+
+# neither gym member and neither developper
+result = set(employees).difference(gym_members, developpers)
+print(result)
+
+
+cpy = developpers.copy()
+cpy.add('Lucy')
+
+print(cpy)
+print(developpers)
