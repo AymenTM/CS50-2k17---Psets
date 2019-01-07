ce80a76006c1599961947a8b04e5c4799eb9bd68
AymenTM <idev.aymen@gmail.com>
2019-01-07 08:34:50 -0800

    Learning New Stuff

--- 7Pset/playground.py
+++ 7Pset/playground.py
@@ -1,140 +1,385 @@
 
-# Iterators  & Iterables
+import itertools
+
+# counter = itertools.count(start=0, step=1)
+
+# >>> print(next(counter))
+# >>> print(next(counter))
+# >>> print(next(counter))
+# >>> print(next(counter))
+
+# 0
+# 1
+# 2
+# 3
 
 # — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
 
+# data = [100, 200, 300, 400]
 
-class myRange:
+# daily_data = list(zip(itertools.count(), data))
 
-    def __init__(self, start, end):
-        self.value = start
-        self.end = end
+# >>> print(daily_data)
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.value >= self.end:
-            raise StopIteration
-        current = self.value
-        self.value += 1
-        return current
-
-
-# example of an object that is both an iterator and iterable
-# 1) we can iterate through its values
-# 2) it remembers where it left off in the iteration and can get the next value in the iterable
-# nums = myRange(3, 10)
-
-# for i in nums:
-# print(i)
-
-
-# print(next(nums))
-# print(next(nums))
-# print(next(nums))
-# print(next(nums))
-# print(next(nums))
-# print(next(nums))
-# ...
-
+# [(0, 100), (1, 200), (2, 300), (3, 400)]
 
 # — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
 
-def myRangeGenerator(current, end):
-    while current < end:
-        yield current
-        current += 1
+# cycle_counter = itertools.cycle([1, 2, 3])
 
+# >>> print(next(cycle_counter))
+# >>> print(next(cycle_counter))
+# >>> print(next(cycle_counter))
+# >>> print(next(cycle_counter))
+# >>> print(next(cycle_counter))
+# >>> print(next(cycle_counter))
 
-# nums = myRangeGenerator(1, 10)
+# 1
+# 2
+# 3
+# 1
+# 2
+# 3
 
-# for i in nums:
-#     print(i)
+# switch = itertools.cycle(['On', 'Off'])
 
+# >>> print(next(switch))
+# >>> print(next(switch))
+# >>> print(next(switch))
+# >>> print(next(switch))
+# >>> print(next(switch))
+# >>> print(next(switch))
 
-# print(next(nums))
-# print(next(nums))
-# print(next(nums))
-# print(next(nums))
-# print(next(nums))
-# print(next(nums))
-# ...
+# 'On'
+# 'Off'
+# 'On'
+# 'Off'
+# 'On'
+# 'Off'
 
 
 # — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
 
-# Sentence Iterable & Iterator Coding Challenge
+# squares = list(map(pow, range(1, 11), itertools.repeat(2)))
+
+# >>> print(squares)
+
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# squares = list(itertools.starmap(pow, list(zip(range(4), itertools.repeat(2)))))
+
+# print(squares)
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# perms = list(itertools.permutations(range(0, 4)))
+
+# >>> print(perms)
+
+# [(0, 1, 2, 3), (0, 1, 3, 2), (0, 2, 1, 3), (0, 2, 3, 1), (0, 3, 1, 2), (0, 3, 2, 1),
+# (1, 0, 2, 3), (1, 0, 3, 2), (1, 2, 0, 3), (1, 2, 3, 0), (1, 3, 0, 2), (1, 3, 2, 0),
+# (2, 0, 1, 3), (2, 0, 3, 1), (2, 1, 0, 3), (2, 1, 3, 0), (2, 3, 0, 1), (2, 3, 1, 0),
+# (3, 0, 1, 2), (3, 0, 2, 1), (3, 1, 0, 2), (3, 1, 2, 0), (3, 2, 0, 1), (3, 2, 1, 0)]
 
 
-# we expect a string of words as argument
-# when we loop over the sentence; we loop over the words in the sentence.
+# perms = list(itertools.permutations(range(0, 3), 2))
+
+# >>> print(perms)
+
+# [(0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1)]
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# combs = list(itertools.combinations(range(0, 3), 2))
+
+# >>> print(combs)
+
+# [(0, 1), (0, 2), (1, 2)]
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# perms_with_repeats = list(itertools.product('ABCDEFGHIJKLMN0PQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', repeat=5))
+
+# for comb in perms_with_repeats:
+#     print(f"{''.join(comb)}")
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# combs_with_reps = list(itertools.combinations_with_replacement('01', 4))
+
+# >>> for comb in combs_with_reps:
+# ...    print(f"{''.join(comb)}")
+
+# 0000
+# 0001
+# 0011
+# 0111
+# 1111
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# perms_with_reps = list(itertools.product('01', repeat=4))
 
 
-class Sentence:
+# >>> for comb in perms_with_reps:
+# ...    print(f"{''.join(comb)}")
 
-    def __init__(self, str):
-        self.str = str
-        self.i = 0
-        self.word_list = str.split(' ')
-        self.len = len(self.word_list)
+# 0000
+# 0001
+# 0010
+# 0011
+# 0100
+# 0101
+# 0110
+# 0111
+# 1000
+# 1001
+# 1010
+# 1011
+# 1100
+# 1101
+# 1110
+# 1111
 
-    def __iter__(self):
-        return self
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
 
-    def __len__(self):
-        return self.len
+# letters = ['a', 'b', 'c', 'd']
+# numbers = [1, 2, 3, 4]
+# names = ['Alex', 'Bob', 'Carl', 'David']
 
-    def __next__(self):
-        if self.i >= self.len:
-            raise StopIteration
-        i = self.i
-        self.i += 1
-        return self.word_list[i]
-
-    def __str__(self):
-        return self.str
+# combined = list(itertools.chain(letters, numbers, names))
 
 
-mystr = Sentence('this is the world of league')
+# >>> print(combined)
 
-# for word in mystr:
-#     print(word)
+# ['a', 'b', 'c', 'd', 1, 2, 3, 4, 'Alex', 'Bob', 'Carl', 'David']
 
-# print(next(mystr))
-# print(next(mystr))
-# print(next(mystr))
-# print(next(mystr))
-# print(next(mystr))
-# print(next(mystr))
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# Getting certain lines of a file
+
+# with open('playground.py', mode='r') as f:
+
+#     desired_lines = itertools.islice(f, 1, 10)
+
+#     for line in desired_lines:
+#         print(line, end='')
 
 
 # — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
 
-# My Solution
-def strWordGenerator(str):
-    word_list = str.split(' ')
-    for word in word_list:
-        yield word_list[i]
-        i += 1
-
-# Corey's solution
-def strWordGenerator(str):
-    for word in str.split():
-        yield word
+# Keep only those who passed
 
 
-str_gen = strWordGenerator('this is the world of league')
+# students = ['Tim', 'Sam', 'Becky', 'John']
+# passed = [True, False, True, True]
 
-# for word in str_gen:
-#     print(word)
+# graduates = list(itertools.compress(students, passed))
 
-print(next(str_gen))
-print(next(str_gen))
-print(next(str_gen))
-print(next(str_gen))
-print(next(str_gen))
-print(next(str_gen))
+# >>> print(graduates)
+
+# ['Tim', 'Becky', 'John']
+
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# Keep only those who passed
+
+# def selector(element):
+#     if element <= 3:
+#         return True
+#     return False
+
+
+# numbers = [2, 4, 1, 5, 3, 6]
+
+
+# selected = list(filter(selector, numbers))
+
+# >>> print(selected)
+
+# [2, 1, 3]
+
+
+# Keep only those who didn't passed
+
+# selected = list(itertools.filterfalse(selector, numbers))
+
+# >>> print(selected)
+
+# [4, 5, 6]
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+
+# def selector(element):
+#     if element <= 5:
+#         return True
+#     return False
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+# filtered = list(itertools.takewhile(selector, numbers))
+# reverse_filtered = list(itertools.dropwhile(selector, numbers))
+
+# >>> print(filtered)
+# >>> print(reverse_filtered)
+
+# [1, 2, 3, 4, 5]
+# [6, 7, 8, 9, 10]
+
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# Accumulate Values of an Iterable (with addition)
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# results = list(itertools.accumulate(numbers))
+
+# >>> print(results)
+
+# [1, 3, 6, 10, 15, 21, 28, 36, 45, 55]
+
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+
+# Accumulate Values of an Iterable (with multiplication)
+
+
+# import operator
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# results = list(itertools.accumulate(numbers, operator.sub))
+
+
+# >>> print(results)
+
+# [1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800]
+
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# List of Dictionaries representing studtents sorted by grade
+students = [
+    {
+        'name': 'Toby Jackson',
+        'year': 'Sophmore',
+        'grade': 'A'
+    },
+    {
+        'name': 'Sam Gowchni',
+        'year': 'Freshman',
+        'grade': 'A'
+    },
+    {
+        'name': 'Marc Salvi',
+        'year': 'Senior',
+        'grade': 'A'
+    },
+    {
+        'name': 'Jerry Polgy',
+        'year': 'Freshman',
+        'grade': 'A'
+    },
+    {
+        'name': 'Ralph Berret',
+        'year': 'Senior',
+        'grade': 'B'
+    },
+    {
+        'name': 'Ralph Berret',
+        'year': 'Senior',
+        'grade': 'B'
+    },
+    {
+        'name': 'Bruno Selko',
+        'year': 'Sophmore',
+        'grade': 'B'
+    },
+    {
+        'name': 'Louis Philips',
+        'year': 'Sophmore',
+        'grade': 'C'
+    },
+    {
+        'name': 'George Derljo',
+        'year': 'Freshman',
+        'grade': 'C'
+    }
+]
+
+
+def get_grade(student):
+    return student['grade']
+
+
+groupings = itertools.groupby(students, key=get_grade)
+
+
+for key, group in groupings:
+    print(f"There are {len(list(group))} students with {key}'s.")
+
+
+# # Output
+
+# "There are 4 students with A's."
+# "There are 3 students with B's."
+# "There are 2 students with C's."
+
+
+# for key, group in groupings:
+#     for student in group:
+#         if key == 'A':
+#             a_ = 'an'
+#         else:
+#             a_ = 'a'
+#         print(f'{student["name"]} is a {student["year"]} and got {a_} {key} on his finals.')
+#     print()
+
+#  # Output
+
+# 'Toby Jackson is a Sophmore and got an A on his finals.'
+# 'Sam Gowchni is a Freshman and got an A on his finals.'
+# 'Marc Salvi is a Senior and got an A on his finals.'
+# 'Jerry Polgy is a Freshman and got an A on his finals.'
+
+# 'Ralph Berret is a Senior and got a B on his finals.'
+# 'Ralph Berret is a Senior and got a B on his finals.'
+# 'Bruno Selko is a Sophmore and got a B on his finals.'
+
+# 'Louis Philips is a Sophmore and got a C on his finals.'
+# 'George Derljo is a Freshman and got a C on his finals.'
+
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# Making duplicates of iterables
+
+
+# numbers = [1, 2, 3, 4, 5]
+
+# copy1, copy2, copy3, copy4, copy5 = itertools.tee(numbers, 5)
+
+
+# >>> print(list(copy1))
+# >>> print(list(copy2))
+# >>> print(list(copy3))
+# >>> print(list(copy4))
+# >>> print(list(copy5))
+
+# [1, 2, 3, 4, 5]
+# [1, 2, 3, 4, 5]
+# [1, 2, 3, 4, 5]
+# [1, 2, 3, 4, 5]
+# [1, 2, 3, 4, 5]
 
 
 # — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
