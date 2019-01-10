fe87575c2026be0bd2c3717dbd361f3b6bced14b
AymenTM <idev.aymen@gmail.com>
2019-01-10 14:07:27 -0800

    Unimportant Change or Tiny Tweak

--- 7Pset/PythonArchive/employeeClass.py
+++ 7Pset/PythonArchive/employeeClass.py
@@ -1,4 +1,6 @@
 
+import requests
+
 class Employee:
 
     num_employees = 0
@@ -42,6 +44,13 @@ class Employee:
         if day.weekday() == 5 or day.weekday() == 6:
             return False
         return True
+
+    def monthly_schedule(self, month):
+        response = requests.get(f'http://company.com/{self._last}/{month}')
+        if response.ok:
+            return response.text
+        else:
+            return 'Bad Response!'
 
     def __repr__(self):
         return (f'Employee({self._first}, {self._last}, {self._pay})')
--- 7Pset/playground.py
+++ 7Pset/playground.py
@@ -1,74 +1,136 @@
 
-from bs4 import BeautifulSoup
-from robobrowser import RoboBrowser
-
-import re
-import os
-import csv
-import datetime
-
-from piscine import USERNAMES
+# UNIT TESTING w/ the 'unittest' Module
 
 
-# Get Piscine Month & Year — — — — — — — — — — — — — — — — — — —
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
 
-month = datetime.date.today().strftime('%B')
-year = datetime.date.today().year
-piscine = f'{month}_Piscine_{year}'
-
-
-# Login to a 42 Account — — — — — — — — — — — — — — — — — — — — —
-
-browser = RoboBrowser(parser='html5lib')
-browser.open('https://signin.intra.42.fr/users/sign_in')
-
-form = browser.get_form()
-form['user[login]'] = os.environ.get('USER')
-form['user[password]'] = os.environ.get('PASS')
-
-browser.submit_form(form)
+"""File called: myOperations. It contains functions we
+want to test"""
 
 
-# Retrieve and Store User Info — — — — — — — — — — — — — — — — —
-
-all_user_info = []
-
-for username in USERNAMES:
-
-    source = browser.open(f'https://profile.intra.42.fr/users/{username}')
-    source = str(browser.parsed())
-    soup = BeautifulSoup(source, 'html5lib')
-
-    match = re.search(r'"Piscine C":{"level":\d\.\d\d?', str(soup))
-    level = re.search(r'\d\.\d\d?', match[0])
-
-    all_user_info.append(
-        {
-            'username': username,
-            'level': float(level[0])
-        }
-    )
+# def add(x, y):
+#     """Add Function"""
+#     return x + y
 
 
-# Sort Users by Level — — — — — — — — — — — — — — — — — — — — —
-
-def get_user_lvl(user):
-    return user['level']
-
-
-all_user_info.sort(key=get_user_lvl, reverse=True)
+# def sub(x, y):
+#     """Subtract Function"""
+#     return x - y
 
 
-# Write the Result to a CSV File — — — — — — — — — — — — — — — —
+# def mul(x, y):
+#     """Multiply Function"""
+#     return x * y
 
-rank = 1
 
-with open(f'{piscine}_Rankings.csv', 'w') as f:
+# def div(x, y):
+#     """Divide Function"""
+#     if y == 0:
+#     	raise ValueError(" Can not divde by zero!")
+#     return x / y
 
-    csv_writer = csv.writer(f, delimiter=',')
 
-    csv_writer.writerow(['Rank', 'Username', 'Level'])
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
 
-    for user in all_user_info:
-        csv_writer.writerow([rank, user['username'], f"{user['level']:.2f}"])
-        rank += 1
+
+"""Test File (seperate from 'myOperations' the file containing
+the functions)"""
+
+
+# import unittest
+# import myOperations
+
+
+# class TestCalc(unittest.TestCase):
+
+#     def test_add(self):
+#         self.assertEqual(myOperations.add(10, 5), 15)
+#         self.assertEqual(myOperations.add(4, 5), 9)
+#         self.assertEqual(myOperations.add(-1, 1), 0)
+#         self.assertEqual(myOperations.add(-1, -1), -2)
+
+#     def test_sub(self):
+#         self.assertEqual(myOperations.sub(10, 5), 5)
+#         self.assertEqual(myOperations.sub(4, 5), -1)
+#         self.assertEqual(myOperations.sub(-1, 1), -2)
+#         self.assertEqual(myOperations.sub(-1, -1), 0)
+
+#     def test_mul(self):
+#         self.assertEqual(myOperations.mul(10, 5), 50)
+#         self.assertEqual(myOperations.mul(4, 5), 20)
+#         self.assertEqual(myOperations.mul(-1, 1), -1)
+#         self.assertEqual(myOperations.mul(-1, -1), 1)
+
+#     def test_div(self):
+#         self.assertEqual(myOperations.div(10, 5), 2)		# Tests for output
+#         self.assertEqual(myOperations.div(4, 5), 0.8)
+#         self.assertEqual(myOperations.div(-1, 1), -1)
+#         self.assertEqual(myOperations.div(-1, -1), 1)
+#         self.assertEqual(myOperations.div(5, 2), 2.5)
+
+#         with self.assertRaises(ValueError):					# Tests for Execption
+#             myOperations.div(10, 0)
+
+
+# if __name__ == '__main__':
+#     unittest.main()
+
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# Employee Class Unit Test
+
+
+import unittest
+from employeeClass import Employee
+
+
+class TestEmployeeClass(unittest.TestCase):
+
+    def setUp(self):
+        self.emp_1 = Employee('Sam', 'Telji', 50000)
+        self.emp_2 = Employee('Xavier', 'Hubert', 60000)
+
+    def tearDown(self):
+        pass
+
+    def test_email(self):
+
+        self.assertEqual(self.emp_1.email, 'sam.telji@email.com')
+        self.assertEqual(self.emp_2.email, 'xavier.hubert@email.com')
+
+        self.emp_1._first = 'John'
+        self.emp_2._last = 'Bruno'
+
+        self.assertEqual(self.emp_1.email, 'john.telji@email.com')
+        self.assertEqual(self.emp_2.email, 'xavier.bruno@email.com')
+
+
+    def test_fullname(self):
+
+        self.assertEqual(self.emp_1.fullname, 'Sam Telji')
+        self.assertEqual(self.emp_2.fullname, 'Xavier Hubert')
+
+        self.emp_1._first = 'John'
+        self.emp_2._last = 'Bruno'
+
+        self.assertEqual(self.emp_1.fullname, 'John Telji')
+        self.assertEqual(self.emp_2.fullname, 'Xavier Bruno')
+
+
+    def test_raise(self):
+
+        self.assertEqual(self.emp_1._pay, 50000)
+        self.assertEqual(self.emp_2._pay, 60000)
+
+        self.emp_1.apply_raise()
+        self.emp_2.apply_raise()
+
+        self.assertEqual(self.emp_1._pay, 52000)
+        self.assertEqual(self.emp_2._pay, 62400)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
