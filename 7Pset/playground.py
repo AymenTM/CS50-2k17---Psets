2bd26de2b6f8d7057043416cec80b398c230a46a
AymenTM <idev.aymen@gmail.com>
2019-01-13 19:34:24 -0800

    Learning New Stuff

--- 7Pset/playground.py
+++ 7Pset/playground.py
@@ -1,7 +1,9 @@
 
+
+
 # MORE GENERATORS & GENERATOR EXPRESSIONS
 
-# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
 
 # def gen_nums(start=0, step=1):
 
@@ -32,7 +34,7 @@
         # 18
         # 20
 
-# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
 
 # def gen_nums(start=0, end=51, step=1):
 
@@ -49,7 +51,7 @@
 
 # [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50]
 
-# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
 
 # GENERATOR EXPRESSION
 # stepper = (x for x in range(10, 51, 2))
@@ -60,19 +62,43 @@
 # <generator object <genexpr> at 0x100a15390>
 # [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50]
 
-# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
 
-stepper = (x for x in range(0, 11, 2))
+# stepper = (x for x in range(0, 11, 2))
 
-print(list(stepper))
+# print(list(stepper))
 
 # [0, 2, 4, 6, 8, 10]
 
 
-stepper = (x * 5 for x in range(0, 10))
+# stepper = (x * 5 for x in range(0, 10))
 
-print(list(stepper))
+# print(list(stepper))
 
 # [0, 5, 10, 15, 20, 25, 30, 35, 40, 45]
 
-# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+# PARTIAL Functions
+
+from functools import partial
+
+
+def say_my_full_name(first, last):
+
+    print(f'{first} {last}')
+
+
+smiths = partial(say_my_full_name, last='Smith')
+
+smiths('John')
+smiths('Tim')
+smiths('Casper')
+smiths('Lula')
+
+# John Smith
+# Tim Smith
+# Casper Smith
+# Lula Smith
+
+# — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
